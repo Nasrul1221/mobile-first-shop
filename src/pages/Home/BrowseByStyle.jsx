213e69895ffb07600045b48b16514f242aba092a
nasrul1221 <fst20071221@gmail.com>
2025-07-16 22:29:50 +0300

    Refactor code structure for improved readability and maintainability

--- src/pages/Home/BrowseByStyle.jsx
+++ src/pages/Home/BrowseByStyle.jsx
@@ -2,21 +2,51 @@ import React from "react";
 
 export default function BrowseByStyle() {
   return (
-    <div className="bg-[#F0F0F0] m-3 sm:m-12 md:m-16 p-4 sm:p-6 md:p-8 rounded-lg text-center">
+    <div className="bg-[#F0F0F0] m-3 sm:m-12 md:m-16 p-4 sm:p-6 md:p-16 rounded-lg text-center">
       <h1 className="font-extrabold text-[32px] md:text-5xl mb-4 md:mb-10">
         BROWSE BY DRESS STYLE
       </h1>
-      <div className="grid grid-rows-4 gap-2 sm:grid-cols-3 sm:grid-rows-2 md:grid-cols-3 md:grid-rows-2 md:gap-3">
-        <div className="bg-white h-[170px] sm:h-[209px] md:h-[209px] rounded-lg">
-          CASUAL
+      <div className="grid grid-rows-4 gap-2 sm:grid-cols-3 sm:grid-rows-2 md:grid-cols-3 md:grid-rows-2 md:gap-3 text-left">
+        <div className="bg-white h-[170px] sm:h-[209px] md:h-[209px] rounded-lg text-2xl font-semibold md:text-3xl p-[1em] relative">
+          <p className="absolute z-10 text-1xl font-semibold sm:text-xl lg:text-3xl">
+            CASUAL
+          </p>
+          <img
+            alt="A model in casual clothes"
+            src="/casual.png"
+            className="w-full h-full absolute top-0 bottom-0 right-0 left-0 rounded-lg"
+          />
         </div>
-        <div className="bg-white md:col-span-2 sm:col-span-2 md:h-[209px] rounded-lg">
-          CASUAL
+        <div className="bg-white md:col-span-2 sm:col-span-2 md:h-[209px] rounded-lg text-2xl font-semibold md:text-3xl p-[1em] relative">
+          <p className="absolute z-10 text-2xl font-semibold md:text-3xl">
+            Formal
+          </p>
+          <img
+            alt="A model in casual clothes"
+            src="/formal.png"
+            className="w-full h-full absolute top-0 bottom-0 right-0 left-0 rounded-lg"
+          />
         </div>
-        <div className="bg-white md:col-span-2 sm:col-span-2 md:h-[209px] rounded-lg">
-          CASUAL
+        <div className="bg-white md:col-span-2 sm:col-span-2 md:h-[209px] rounded-lg text-2xl font-semibold md:text-3xl p-[1em] relative">
+          <p className="absolute z-10 text-2xl font-semibold md:text-3xl">
+            Party
+          </p>
+          <img
+            alt="A model in casual clothes"
+            src="/party.png"
+            className="w-full h-full absolute top-0 bottom-0 right-0 left-0 rounded-lg"
+          />
         </div>
-        <div className="bg-white md:h-[209px] rounded-lg">CASUAL</div>
+        <div className="bg-white md:h-[209px] rounded-lg p-[1em] relative">
+          <p className="absolute z-10 text-2xl font-semibold md:text-3xl">
+            Gym
+          </p>
+          <img
+            alt="A model in casual clothes"
+            src="/gym.png"
+            className="w-full h-full absolute top-0 bottom-0 right-0 left-0 rounded-lg"
+          />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home/TitlePage.jsx
+++ src/pages/Home/TitlePage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function TitlePage() {
   return (
-    <section className="bg-[#F2F0F1] h-[853px] w-full md:h-[603px] pt-5 pl-5 pr-5 md:pt-16 md:pl-16 md-pr-16 md:grid md:grid-cols-2 gap-x-3">
-      <div>
+    <section className="bg-[#F2F0F1] h-full w-full md:max-h-[663px] pt-5 pl-5 pr-5 md:pt-0 md:pl-10 md:pr-10 md:grid md:grid-cols-2 gap-x-3">
+      <div className="flex flex-col gap-y-16">
         <div>
-          <h1 className="text-4xl md:text-[64px] font-extrabold w-[315px] md:max-w-[577px] md:w-full leading-[34px] md:leading-[64px] mt-7 overflow-hidden">
+          <h1 className="text-4xl md:text-[48px] lg:text-[64px] font-extrabold w-[315px] md:max-w-[577px] md:w-full leading-[34px] md:leading-[64px] mt-7 overflow-hidden">
             FIND CLOTHES THAT MATCHES YOUR STYLE
           </h1>
           <p className="text-sm opacity-60 md:text-base md:max-w-[545px] md:w-full mt-4">
@@ -17,31 +17,33 @@ export default function TitlePage() {
             Shop now
           </button>
         </div>
-        <div className="grid grid-cols-3 md:grid-cols-5 md:grid-rows-1 place-items-center mt-4 gap-y-2 mr-4">
+        <div className="grid grid-cols-3 xl:grid-cols-5 xl:grid-rows-1 place-items-center mt-4 gap-y-2 mr-4 md:mb-4 w-full">
           <div>
-            <strong className="text-2xl">200+</strong>
+            <strong className="text-2xl md:text-4xl">200+</strong>
             <p className="text-xs text-nowrap">International Brands</p>
           </div>
 
           <div className="h-full w-[1px] bg-gray-300"></div>
 
           <div>
-            <strong className="text-2xl">2,000+</strong>
+            <strong className="text-2xl md:text-4xl">2,000+</strong>
             <p className="text-xs text-nowrap">High-Quality Products</p>
           </div>
 
-          <div className="hidden md:block h-full w-[1px] bg-gray-300"></div>
+          <div className="hidden xl:block h-full w-[1px] bg-gray-300"></div>
 
-          <div className="col-span-3 md:col-span-1">
-            <strong className="text-2xl">30,000+</strong>
+          <div className="col-span-3 xl:col-span-1">
+            <strong className="text-2xl md:text-4xl">30,000+</strong>
             <p className="text-xs text-nowrap mb-3 md:mb-0">Happy Customers</p>
           </div>
         </div>
       </div>
-      <div className="w-full max-h-[448px] h-full">
-        <div className="bg-gray-500 w-full h-full flex items-center justify-center">
-          <p className="text-white">Here must be an image</p>
-        </div>
+      <div className="max-h-[663px] max-w-full h-full md:w-full md:h-full">
+        <img
+          alt="Two fashion models pose"
+          src="/titlePage.png"
+          className="h-full w-full"
+        />
       </div>
     </section>
   );
